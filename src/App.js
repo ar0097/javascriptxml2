e1ce8f8f003405a0dd17a3a18c43124d213ecaaa
arbaz shah <arbazshah7454@gmail.com>
2022-11-03 18:38:50 +0530

    Create a React app and publish it to Github Pages

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './asset/airbnb.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Evolution of <br /> Airbnb's Frontend</h1>
+        <h4>@spikebrehm</h4>
+        <img src={logo} alt="" width="25px"/>
+        <h6>airbnb</h6>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
